c5be6ca83825406683f4b8b70e5748140aea9045
Fatou <dchifai8@gmail.com>
2025-03-13 13:11:24 +0000

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) 
+        { 
+            $usertype = Auth::user()->usertype; 
+           
+            if ($usertype == 'admin') {
+              
+                return view ('admin/index'); 
+           
+            } else {
+           
+                return view('dashboard');
+           
+            } 
+       
+        }
+
+        return redirect()->route('login'); // Redirige vers la connexion si non authentifié
+    }
+}
